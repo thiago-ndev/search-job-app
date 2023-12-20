401a780f86d23b94c60f154a3c8617867bff64e6
Thiago Alves <thiago.ndev@gmail.com>
2023-12-20 02:07:18 -0300

    test-deploy: 3.6

--- Job_Project/settings.py
+++ Job_Project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Search_Job', 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 # Media files
 MEDIA_URL = 'media/'
