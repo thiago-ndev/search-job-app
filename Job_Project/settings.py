f5a3a0ec388a07e1357744826776c4557d7bc006
Thiago Alves <thiago.ndev@gmail.com>
2023-12-20 17:15:39 -0300

    test-deploy: 4.2

--- Job_Project/settings.py
+++ Job_Project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gq^v^b8_4r1m_t9628u%7i8y^mx@d6%5%noviagh9ov8x5++(h'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
--- Search_Job/views.py
+++ Search_Job/views.py
@@ -7,9 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def custom_404(request, exception):
-    return render(request, '404.html', {}, status=404)
-
 def filtrar_job(request):
     form = FiltredForm(request.POST)
 
