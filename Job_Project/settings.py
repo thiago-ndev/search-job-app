a7539416366c9672a59f1adebb5e28bce611aaf6
Thiago Alves <thiago.ndev@gmail.com>
2023-12-20 17:42:21 -0300

    test-deploy: 4.3

--- Job_Project/settings.py
+++ Job_Project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-gq^v^b8_4r1m_t9628u%7i8y^mx@d6%5%noviagh9ov8x5++(h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://search-job.up.railway.app/"]
+ALLOWED_HOSTS = ["search-job.up.railway.app/"]
 
 
 # Application definition
