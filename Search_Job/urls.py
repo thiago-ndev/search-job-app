dbaae696a9b6b5ea2c6cf3a429a9326d6d40015d
Thiago Alves <thiago.ndev@gmail.com>
2024-02-11 14:59:32 -0300

    test-deploy: 5.3

--- Search_Job/urls.py
+++ Search_Job/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'Search_Job'
 
-handler404 = "Search_Job.views.my_custom_page_not_found_view"
+#handler404 = "Search_Job.views.my_custom_page_not_found_view"
 
-handler403 = "Search_Job.views.my_custom_session_expired_view"
+#handler403 = "Search_Job.views.my_custom_session_expired_view"
 
 urlpatterns = [
     path('', views.filtrar_job, name='home'),
